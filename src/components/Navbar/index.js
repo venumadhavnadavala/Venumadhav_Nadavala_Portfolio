7a592108b03ecfa8aed35db107762371c49b6a39
venumadhavnadavala <venumadhavnadavala@gmail.com>
2026-05-20 14:39:09 +0530

    ccc

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,17 @@
-import React from 'react'
-import { Nav, NavLink, NavbarContainer, Span, NavLogo, NavItems, GitHubButton, ButtonContainer, MobileIcon, MobileMenu, MobileLink } from './NavbarStyledComponent'
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  NavbarContainer,
+  Span,
+  NavLogo,
+  NavItems,
+  GitHubButton,
+  ButtonContainer,
+  MobileIcon,
+  MobileMenu,
+  MobileLink,
+} from './NavbarStyledComponent';
 import { DiCssdeck } from 'react-icons/di';
 import { FaBars } from 'react-icons/fa';
 import { Bio } from '../../data/constants';
@@ -12,44 +24,95 @@ const Navbar = () => {
   return (
     <Nav>
       <NavbarContainer>
-        <NavLogo to='/'>
-          <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20', cursor: 'pointer' }}>
+        {/* Fixed: Removed nested <a> tag inside NavLogo */}
+        <NavLogo to="/">
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              color: 'white',
+              marginBottom: '20px',
+              cursor: 'pointer',
+            }}
+          >
             <DiCssdeck size="3rem" color={theme.primary} />
             <Span>Portfolio</Span>
-          </a>
+          </div>
         </NavLogo>
+
         <MobileIcon>
           <FaBars onClick={() => setIsOpen(!isOpen)} />
         </MobileIcon>
+
         <NavItems>
           <NavLink href="#about">About</NavLink>
-          <NavLink href='#skills'>Skills</NavLink>
-          <NavLink href='#experience'>Experience</NavLink>
-          <NavLink href='#projects'>Projects</NavLink>
-          <NavLink href='#education'>Education</NavLink>
+          <NavLink href="#skills">Skills</NavLink>
+          <NavLink href="#experience">Experience</NavLink>
+          <NavLink href="#projects">Projects</NavLink>
+          <NavLink href="#education">Education</NavLink>
         </NavItems>
+
         <ButtonContainer>
-          <GitHubButton href={Bio.github} target="_blank">Github Profile</GitHubButton>
+          <GitHubButton
+            href={Bio.github}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github Profile
+          </GitHubButton>
         </ButtonContainer>
-        {isOpen &&
+
+        {isOpen && (
           <MobileMenu isOpen={isOpen}>
-            <MobileLink href="#about" onClick={() => setIsOpen(!isOpen)}>About</MobileLink>
-            <MobileLink href='#skills' onClick={() => setIsOpen(!isOpen)}>Skills</MobileLink>
-            <MobileLink href='#experience' onClick={() => setIsOpen(!isOpen)}>Experience</MobileLink>
-            <MobileLink href='#projects' onClick={() => setIsOpen(!isOpen)}>Projects</MobileLink>
-            <MobileLink href='#education' onClick={() => setIsOpen(!isOpen)}>Education</MobileLink>
+            <MobileLink
+              href="#about"
+              onClick={() => setIsOpen(false)}
+            >
+              About
+            </MobileLink>
+            <MobileLink
+              href="#skills"
+              onClick={() => setIsOpen(false)}
+            >
+              Skills
+            </MobileLink>
+            <MobileLink
+              href="#experience"
+              onClick={() => setIsOpen(false)}
+            >
+              Experience
+            </MobileLink>
+            <MobileLink
+              href="#projects"
+              onClick={() => setIsOpen(false)}
+            >
+              Projects
+            </MobileLink>
+            <MobileLink
+              href="#education"
+              onClick={() => setIsOpen(false)}
+            >
+              Education
+            </MobileLink>
+
             <GitHubButton
-              style={{ padding: '10px 16px', background: `${theme.primary}`, color: 'white', width: 'max-content' }}
+              style={{
+                padding: '10px 16px',
+                background: theme.primary,
+                color: 'white',
+                width: 'max-content',
+              }}
               href={Bio.github}
               target="_blank"
+              rel="noopener noreferrer"
             >
               Github Profile
             </GitHubButton>
           </MobileMenu>
-        }
+        )}
       </NavbarContainer>
     </Nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
